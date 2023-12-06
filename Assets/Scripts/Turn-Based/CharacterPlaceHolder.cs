e61bcdbc2a24e16391015466291ee27ad4e72aa5
Álvaro R. Acosta <alvaro_ac3@outlook.es>
2023-12-06 20:19:42 +0100

    Unión Lucas y mío

--- Assets/Scripts/Turn-Based/CharacterPlaceHolder.cs
+++ Assets/Scripts/Turn-Based/CharacterPlaceHolder.cs
@@ -1,86 +1,104 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CharacterPlaceHolder : MonoBehaviour
 {
-    public string characterName;
+    
     public float advanceDistance = 5f;
     public float rotateAngle = 90f; // Ángulo de giro en grados
     public float dashDistance = 10f;
     public float movementSpeed = 5f;
 
+    //Nombres
+    public string characterName;
+    public string firstSkill;
+    public string secondSkill;
+    public string thirdSkill;
+
     //Importantes
     private bool isMoving = false;
     private bool movementCompleted = false;
 
-    public IEnumerator MoveForward()
+    //Lucas
+    public bool isPlayerControlled;
+
+    public List<Character> selectedCharacters = new List<Character>();
+    public Transform selectedGroundPosition;
+    public bool selectionFinished = false;
+    const int MaxHealth = 100;
+
+    public int health = MaxHealth;
+
+    GameManager gm;
+    private Renderer rend;
+    Color highlightedColor = Color.green;
+
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        gm = FindObjectOfType<GameManager>();
+    }
+
+    //Esta función comprueba si es enemigo o aliado
+    public void checkStatus()
+    {
+        if (isPlayerControlled)
+        {
+            // show control interface
+        }
+    }
+
+
+    public void Move()
     {
         if (!isMoving)
         {
             isMoving = true;
             movementCompleted = false;
-            Vector3 newPosition = transform.position + transform.forward * advanceDistance;
-            yield return StartCoroutine(MoveGradually(newPosition));
+            print(characterName + " has moved");
             isMoving = false;
             movementCompleted = true;
         }
     }
 
-    public IEnumerator Rotate()
+    public void Rotate()
     {
         if (!isMoving)
         {
             isMoving = true;
             movementCompleted = false;
-            Quaternion newRotation = transform.rotation * Quaternion.Euler(0f, rotateAngle, 0f);
-            yield return StartCoroutine(RotateGradually(newRotation));
+            print(characterName + " has rotated");
             isMoving = false;
             movementCompleted = true;
         }
     }
 
-    public IEnumerator Dash()
+    public void Dash()
     {
         if (!isMoving)
         {
             isMoving = true;
             movementCompleted = false;
-            Vector3 dashPosition = transform.position + transform.forward * dashDistance;
-            yield return StartCoroutine(MoveGradually(dashPosition));
+            print(characterName + " has dashed");
             isMoving = false;
             movementCompleted = true;
         }
     }
 
-    private IEnumerator MoveGradually(Vector3 destination)
+    public void Slide()
     {
-        float startTime = Time.time;
-        Vector3 initialPosition = transform.position;
-
-        while (Time.time - startTime < advanceDistance / movementSpeed)
+        if (!isMoving)
         {
-            float t = (Time.time - startTime) / (advanceDistance / movementSpeed);
-            transform.position = Vector3.Lerp(initialPosition, destination, t);
-            yield return null;
+            isMoving = true;
+            movementCompleted = false;
+            print(characterName + " has slided");
+            isMoving = false;
+            movementCompleted = true;
         }
-
-        transform.position = destination;
     }
 
-    private IEnumerator RotateGradually(Quaternion targetRotation)
-    {
-        float startTime = Time.time;
-        Quaternion initialRotation = transform.rotation;
-
-        while (Time.time - startTime < 1f)
-        {
-            float t = (Time.time - startTime);
-            transform.rotation = Quaternion.Slerp(initialRotation, targetRotation, t);
-            yield return null;
-        }
-
-        transform.rotation = targetRotation;
-    }
 
     public bool IsMoving()
     {
@@ -96,5 +114,20 @@ public class CharacterPlaceHolder : MonoBehaviour
     {
         isMoving = false;
         movementCompleted = false;
+    }
+
+    public virtual void PerformAction1()
+    {
+        Rotate();
+    }
+
+    public virtual void PerformAction2()
+    {
+        Dash();
+    }
+
+    public virtual void PerformAction3()
+    {
+        Slide();
     }
 }
--- Assets/Scripts/Turn-Based/GameManager.cs
+++ Assets/Scripts/Turn-Based/GameManager.cs
@@ -5,52 +5,79 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public TMP_Text turnText;
+
+    //Listas de enemigos y aliados
     public GameObject alliesContainer;
     public GameObject enemiesContainer;
-    public TMP_Text turnText;
+
 
     private List<CharacterPlaceHolder> allies = new List<CharacterPlaceHolder>();
     private List<CharacterPlaceHolder> enemies = new List<CharacterPlaceHolder>();
+
+    public List<CharacterPlaceHolder> charactersList;
+
     private int activeCharacterIndex = 0;
 
-    public CharacterController characterController;
+    public LayerMask ground;
 
     void Start()
     {
-        PopulateList(alliesContainer, allies);
-        PopulateList(enemiesContainer, enemies);
+        charactersList = generateList(allies, enemies);
         UpdateTurnText();
         StartTurn();
     }
 
     void Update()
     {
-        //print("Se mueve " + GetCurrentCharacter().IsMoving());                        //Debug
-        //print("Se ha movido " + GetCurrentCharacter().IsMovementCompleted());         //Debug
+        if (!GetCurrentCharacter().selectionFinished)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3 clickPosition = -Vector3.one;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, 100f, ground))
+                {
+                    GetCurrentCharacter().selectedGroundPosition = hit.transform; // TODO: check if correct
+                    Debug.Log(GetCurrentCharacter().selectedGroundPosition);
+                }
+            }
+        }
+
+
+
         if (IsTurnComplete())
         {
             EndTurn();
         }
     }
 
+    //Comprueba si ha terminado el turno
     private bool IsTurnComplete()
     {
         return GetCurrentCharacter().IsMovementCompleted();
     }
 
+    //Inicia un nuevo turno
     private void StartTurn()
     {
         // Reiniciar el estado de movimiento solo para el personaje actual
         GetCurrentCharacter().ResetMovementStatus();
-        characterController.SetCharacter(GetCurrentCharacter());
+
+        GetCurrentCharacter().checkStatus();
+
+
     }
 
+    //Termina el turno y pasa al siguiente
     private void EndTurn()
     {
         // Pasar al siguiente personaje
         activeCharacterIndex++;
 
-        if (activeCharacterIndex >= allies.Count + enemies.Count)
+        if (activeCharacterIndex >= charactersList.Count)
         {
             activeCharacterIndex = 0;
             StartTurn();  // Iniciar un nuevo turno cuando se completa un ciclo de turnos
@@ -60,38 +87,82 @@ public class GameManager : MonoBehaviour
         StartTurn();
     }
 
+    //Genera la lista de personajes
+    private List<CharacterPlaceHolder> generateList(List<CharacterPlaceHolder> list1, List<CharacterPlaceHolder> list2)
+    {
+        PopulateList(alliesContainer, allies);
+        PopulateList(enemiesContainer, enemies);
+        List<CharacterPlaceHolder> list = new List<CharacterPlaceHolder>(list1);
+        list.AddRange(list2);
+        ShuffleList(list);
+        return list;
+    }
+    
     private void PopulateList(GameObject container, List<CharacterPlaceHolder> list)
     {
         CharacterPlaceHolder[] characters = container.GetComponentsInChildren<CharacterPlaceHolder>();
         list.AddRange(characters);
     }
 
+    //Esta función se encarga de poner el texto por pantalla
     private void UpdateTurnText()
     {
         turnText.text = $"Turno de: {GetCurrentCharacterName()}";
     }
 
+    //Esta función se ocupa de coger todos los nombres de Character, bien su nombre principal o bien sus habilidades
     private string GetCurrentCharacterName()
     {
-        if (activeCharacterIndex < allies.Count)
+        return charactersList[activeCharacterIndex].characterName;
+    }
+
+    //Esta función devuelve el personaje actual
+    private CharacterPlaceHolder GetCurrentCharacter()
+    {
+        
+        return charactersList[activeCharacterIndex];        
+    }
+
+    //Estas funciones se "comunican" con el personaje y les da instrucciones de lo que hacer
+    public void Move()
+    {
+        GetCurrentCharacter().Move();
+    }
+
+    public void PerformAction1()
+    {
+        GetCurrentCharacter().PerformAction1();
+    }
+
+    public void PerformAction2()
+    {
+        GetCurrentCharacter().PerformAction2();
+
+    }
+
+    public void PerformAction3()
+    {
+        GetCurrentCharacter().PerformAction3();
+    }
+
+
+
+    //Algoritmo para ordenar de forma aleatoria una lista
+
+    void ShuffleList<T>(List<T> lista)
+    {
+        int n = lista.Count;
+        System.Random rng = new System.Random();
+
+        // Aplicar el algoritmo de Fisher-Yates para mezclar la lista
+        while (n > 1)
         {
-            return allies[activeCharacterIndex].characterName;
-        }
-        else
-        {
-            return enemies[activeCharacterIndex - allies.Count].characterName;
+            n--;
+            int k = rng.Next(n + 1);
+            T valor = lista[k];
+            lista[k] = lista[n];
+            lista[n] = valor;
         }
     }
 
-    private CharacterPlaceHolder GetCurrentCharacter()
-    {
-        if (activeCharacterIndex < allies.Count)
-        {
-            return allies[activeCharacterIndex];
-        }
-        else
-        {
-            return enemies[activeCharacterIndex - allies.Count];
-        }
-    }
 }
