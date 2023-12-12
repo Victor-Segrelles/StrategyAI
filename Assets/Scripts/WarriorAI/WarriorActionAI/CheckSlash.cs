0d72e24a46cdf220765392e05e58833db0752561
al405059 <al405059@uji.es>
2023-12-12 03:05:09 +0100

    Warrior AIActions

---
+++ Assets/Scripts/WarriorAI/WarriorActionAI/CheckSlash.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using BehaviorTree;
+
+public class CheckSlash : NodeBT
+{
+    private Unit unit;
+
+    public CheckSlash(Unit obj)
+    {
+        unit = obj;
+    }
+
+    public override NodeState Evaluate()
+    {
+        object t = GetData("checkSlash");
+        if (t == null || unit.actionTarget == null)
+        {
+            Character odiado = null;
+            float min = Mathf.Infinity;
+
+            foreach (Character ally in unit.gm.allies)
+            {
+                if (Vector3.Distance(unit.gameObject.transform.position, ally.gameObject.transform.position) < unit.attackRange && ally.unit.life < min)
+                {
+                    odiado = ally;
+                    min = ally.unit.life;
+                }
+            }
+
+
+            if (odiado != null)
+            {
+                unit.actionTarget = odiado;
+                unit.selectedAction = 1;    //Attack
+                parent.parent.SetData("checkSlash", unit.actionTarget);
+                state = NodeState.SUCCESS;
+                return state;
+            }
+
+
+            state = NodeState.FAILURE;
+            return state;
+        }
+        state = NodeState.SUCCESS;
+        return state;
+    }
+
+}
